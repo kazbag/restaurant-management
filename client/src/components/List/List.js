14db9e71ec445de2323d420a0760eb3fa0100a58
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-04-18 14:30:43 +0200

    added styles and fake data for products

--- client/src/components/List/List.js
+++ client/src/components/List/List.js
@@ -1,6 +1,19 @@
 import React, { useState, useEffect } from "react";
 
 const List = (props) => {
-  return <div className="list">{props.children}</div>;
+  return (
+    <div
+      className="list"
+      style={{
+        maxWidth: "1366px",
+        backgroundColor: "#f0f0f0",
+        margin: "0 auto",
+        color: "black",
+        borderRadius: "12px",
+      }}
+    >
+      {props.children}
+    </div>
+  );
 };
 export default List;
