08e73f02cedf089c5eb917dcca39ce67b84424df
fkozlowski <filip.kozlowski@microsoft.wsei.edu.pl>
2020-05-14 17:54:40 +0200

    add the about page

---
+++ client/src/pages/AboutPage.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { withRouter } from "react-router";
+import { AuthContext } from "../contexts/AuthContext";
+import "./styles.css";
+import {
+  StyledContainerBox,
+  StyledImageContainerBox,
+  StyledTextContainerBox,
+} from "../stylesComponents/StyledComponents";
+
+const AboutPage = () => {
+  return (
+    <StyledContainerBox>
+      <StyledTextContainerBox>
+        <a>
+          Istniejemy na rynku od ponad 20 lat. Gotowanie jest naszą pasją.
+          Obsługujemy różne wydarzenia (wesela, chrzciny, komunie, stypy,
+          catering), jedzenie z dostawą na terenie Krakowa. W roku 2018
+          zostaliśmy wyróżnieni nagrodą Złotej Miski
+        </a>
+      </StyledTextContainerBox>
+      <StyledImageContainerBox>
+        <img src="https://dziendobry.tvn.pl/media/cache/content/kucharz-i-obsluga-restauracji-jpg.jpg" />
+      </StyledImageContainerBox>
+    </StyledContainerBox>
+  );
+};
+export default withRouter(AboutPage);
