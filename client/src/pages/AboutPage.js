468d8fd01d182605983e4e8d448191ebceda1fce
kazbag <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-05-26 23:39:23 +0200

    added countup and styling about page

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,27 +1,70 @@
 import React, { useContext } from "react";
 import { withRouter } from "react-router";
 import "./styles.css";
+import Countup from "react-countup";
 import {
   StyledContainerBox,
   StyledImageContainerBox,
   StyledTextContainerBox,
 } from "../stylesComponents/StyledComponents";
-
+import styled from "styled-components";
 const AboutPage = () => {
   return (
-    <StyledContainerBox>
-      <StyledTextContainerBox>
-        <p>
+    <StyledContainer>
+      <StyledAside>
+        <StyledText>
           Istniejemy na rynku od ponad 20 lat. Gotowanie jest naszą pasją.
           Obsługujemy różne wydarzenia (wesela, chrzciny, komunie, stypy,
           catering), jedzenie z dostawą na terenie Krakowa. W roku 2018
           zostaliśmy wyróżnieni nagrodą Złotej Miski
-        </p>
-      </StyledTextContainerBox>
+        </StyledText>
+        <StyledCountersBox>
+          <StyledInfo>Zadowolonych klientów</StyledInfo>
+          <StyledInfo>Złożonych zamówień</StyledInfo>
+          <StyledInfo>Przejechanych kilometrów</StyledInfo>
+          <StyledInfo>Dni na rynku</StyledInfo>
+          <Countup start={0} end={9213} duration={4.75} />
+          <Countup start={0} end={15733} duration={4.75} />
+          <Countup start={0} end={29214} duration={4.75} />
+          <Countup start={0} end={932} duration={4.75} />
+        </StyledCountersBox>
+      </StyledAside>
       <StyledImageContainerBox>
         <img src="https://dziendobry.tvn.pl/media/cache/content/kucharz-i-obsluga-restauracji-jpg.jpg" />
       </StyledImageContainerBox>
-    </StyledContainerBox>
+    </StyledContainer>
   );
 };
+const StyledAside = styled.div`
+  width: 50%;
+  flex-direction: column;
+  display: flex;
+  justify-content: space-between;
+`;
+const StyledContainer = styled.div`
+  display: flex;
+`;
+const StyledCountersBox = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-auto-rows: minmax(min-content, max-content);
+  text-align: center;
+  grid-row-gap: 2rem;
+  span {
+    color: #b0d332;
+  }
+`;
+const StyledText = styled.p`
+  flex: 1;
+  padding: 2rem;
+  background-color: #d0d0d0;
+  color: #000;
+  margin-bottom: 1rem;
+  border-radius: 0.6rem;
+`;
+const StyledInfo = styled.h3`
+  color: #b0d332;
+`;
+
 export default withRouter(AboutPage);
