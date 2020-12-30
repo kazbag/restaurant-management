13580756a2528dad56fbb01721e7085c32f0f6e0
Kazimierz Bagrowski <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2020-12-30 17:29:07 +0100

    kitchen fixes

--- client/src/pages/KitchenPage.js
+++ client/src/pages/KitchenPage.js
@@ -15,145 +15,34 @@ import {
   StyledDescription,
   StyledSpan,
 } from "../stylesComponents/StyledComponents";
+import {
+  Container,
+  Modal,
+  List,
+} from "../components/Kitchen/kitchen_components";
 import Loader from "../components/Loader/Loader";
-const serverUrl = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
+import { useLoad } from "../utils/hooks";
+const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
 
 const KitchenPage = ({ history }) => {
   const { isAuthenticated, setAuth } = useContext(AuthContext);
-  const [ordersPending, setOrdersPending] = useState([]);
-  const [ordersCompleted, setOrdersCompleted] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [completedItemsValue, setCompletedItemsValue] = useState(0);
-  const [notCompletedItemsValue, setNotCompletedItemsValue] = useState(0);
-  const [pusherLoading, setPusherLoading] = useState(false);
-
-  const getAllOrders = () => {
-    axios
-      .get(`${serverUrl}/orders/completed`, setLoading(true))
-      .then((response) => {
-        setOrdersCompleted(response.data);
-      })
-      .then(() => setLoading(false));
-    axios
-      .get(`${serverUrl}/orders/pending`, setLoading(true))
-      .then((response) => {
-        setOrdersPending(response.data);
-      })
-      .then(() => setLoading(false));
-  };
-
-  const pusher = new Pusher(`${process.env.REACT_APP_PUSHER_KEY}`, {
-    cluster: `${process.env.REACT_APP_PUSHER_CLUSTER}`,
-  });
-  const channel = pusher.subscribe("my-channel");
-  // Pusher.logToConsole = true;
-  useEffect(() => {
-    channel.bind("updated", function(data) {
-      setPusherLoading(data);
-    });
-    channel.bind("inserted", function(data) {
-      setPusherLoading(data);
-    });
-  }, []);
+  const [pendingOrders] = useLoad([], `${SERVER_URL}/orders/pending`);
+  const [completedOrders] = useLoad([], `${SERVER_URL}/orders/completed`);
 
   useEffect(() => {
-    getAllOrders();
-  }, []);
+    console.log(pendingOrders);
+  }, [pendingOrders]);
 
   useEffect(() => {
-    getAllOrders();
-  }, [pusherLoading]);
-
-  useEffect(() => {
-    axios
-      .get(`${serverUrl}/orders/pending`)
-      .then((response) => {
-        setOrdersPending(response.data);
-      })
-      .then(() => setLoading(false));
-  }, []);
-
-  useEffect(() => {
-    console.log("siema");
-  }, [Pusher]);
-
-  const togglePending = (_id) => {
-    axios
-      .patch(`${serverUrl}/orders/status/${_id}`)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const itemsNotCompleted = ordersPending.map((item, index) => {
-    const { _id, phone, price, products, address, time, isCompleted } = item;
-    const productsList = products.map((prod, index) => {
-      return <StyledSpan key={index}>{prod}, </StyledSpan>;
-    });
-    return (
-      <StyledListItem key={(Math.random() * 1000).toString()}>
-        <StyledDescription>
-          #{index + 1} {price} zł: {productsList} {address} {time}
-        </StyledDescription>
-        <StyledListItemHeader>
-          <StyledListItemLink href={`tel:${phone}`}>Zadzwoń</StyledListItemLink>
-          <StyledListItemLink target="_blank" href={`/order/${_id}`}>
-            Pokaż zamówienie
-          </StyledListItemLink>
-          <StyledButton order={_id} onClick={() => togglePending(_id)}>
-            Archiwizuj
-          </StyledButton>
-        </StyledListItemHeader>
-      </StyledListItem>
-    );
-  });
-
-  const itemsCompleted = ordersCompleted.map((item, index) => {
-    const { _id, phone, price, products, address, time, isCompleted } = item;
-    const productsList = products.map((prod, index) => {
-      return <StyledSpan key={index}>{prod}, </StyledSpan>;
-    });
-    return (
-      <StyledListItem key={(Math.random() * 1000).toString()}>
-        <StyledDescription>
-          #{index + 1} {price} zł: {productsList} {address} {time}
-        </StyledDescription>
-        <StyledListItemHeader>
-          <StyledListItemLink href={`tel:${phone}`}>Zadzwoń</StyledListItemLink>
-          <StyledListItemLink target="_blank" href={`/order/${_id}`}>
-            Pokaż zamówienie
-          </StyledListItemLink>
-          <StyledButton order={_id} onClick={() => togglePending(_id)}>
-            Przywróć
-          </StyledButton>
-        </StyledListItemHeader>
-      </StyledListItem>
-    );
-  });
-
+    console.log(completedOrders);
+  }, [completedOrders]);
   return (
     <AuthContext.Consumer>
       {(context) => (
-        <>
-          <Loader loading={loading.toString()} />
-          <StyledContainer>
-            <StyledBox>
-              <StyledHeader>Zamówienia do zrealizowania</StyledHeader>
-              <StyledList>
-                {loading ? <li>ładowanie danych...</li> : itemsNotCompleted}
-              </StyledList>
-            </StyledBox>
-            <StyledBox>
-              <StyledHeader>Zamówienia zrealizowane</StyledHeader>
-              <StyledList>
-                {loading ? <li>ładowanie danych...</li> : itemsCompleted}
-              </StyledList>
-            </StyledBox>
-            <StyledBox>{notCompletedItemsValue} zł</StyledBox>
-            <StyledBox>{completedItemsValue} zł</StyledBox>
-          </StyledContainer>
-        </>
+        <Container>
+          <List data={pendingOrders} header="Do realizacji" />
+          <List data={completedOrders} header="Zrealizowane" />
+        </Container>
       )}
     </AuthContext.Consumer>
   );
