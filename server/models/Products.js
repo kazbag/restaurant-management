3a36edcb52aaeb55d694aee6dd1af3477b9ca8fa
Kazimierz Bagrowski <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2021-01-01 17:43:46 +0100

    remove category from product schema

--- server/models/Products.js
+++ server/models/Products.js
@@ -1,25 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProductsSchema = mongoose.Schema({
-
-    name: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    price: {
-        type: Number,
-        required: true,
-    },
-    description: {
-        type: String,
-    },
-    photo: {
-        type: String,
-        unique: true
-    },
-    category: {
-        type: String,
-    },
-})
-module.exports = mongoose.model('Products', ProductsSchema);
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  photo: {
+    type: String,
+    unique: true,
+  },
+});
+module.exports = mongoose.model("Products", ProductsSchema);
