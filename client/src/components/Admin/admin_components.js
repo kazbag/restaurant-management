0c835161ebb2130a0bd2c00022fe013f59912800
Kazimierz Bagrowski <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2021-01-01 19:08:11 +0100

    user edit

--- client/src/components/Admin/admin_components.js
+++ client/src/components/Admin/admin_components.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 
-export const UserList = ({ users, onEdit, onRemove, isModalVisible }) => {
+export const UserList = ({
+  users,
+  performEdit,
+  onEdit,
+  onRemove,
+  isModalVisible,
+}) => {
   return (
     <div className="col-12 col-md-6">
       <div className="card">
@@ -20,7 +26,7 @@ export const UserList = ({ users, onEdit, onRemove, isModalVisible }) => {
                 <div className="d-flex ml-auto">
                   <button
                     data-id={user._id}
-                    onClick={onEdit}
+                    onClick={performEdit}
                     className="btn btn-sm btn-primary mr-2"
                   >
                     Edytuj
@@ -38,22 +44,118 @@ export const UserList = ({ users, onEdit, onRemove, isModalVisible }) => {
           })}
         </div>
       </div>
-      {isModalVisible && (
-        <div
-          className="card"
-          style={{
-            minWidth: "75vw",
-            position: "fixed",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            zIndex: "100000",
-            boxShadow: "5px 5px rgba(0,0,0,0.3)",
-          }}
-        >
-          eee
-        </div>
-      )}
+    </div>
+  );
+};
+
+export const UserEdit = ({ onSubmit, onCancel, onChange, user, roles }) => {
+  return (
+    <div
+      className="card"
+      style={{
+        minWidth: "75vw",
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        zIndex: "100000",
+        boxShadow: "5px 5px rgba(0,0,0,0.3)",
+      }}
+    >
+      <div className="card-header">
+        <h3 className="card-title">Edycja użytkownika</h3>
+      </div>
+      <div className="card-body">
+        <form className="form">
+          <div className="form-group">
+            <label>Imię</label>
+            <input
+              type="text"
+              className="form-control"
+              value={user.name}
+              name="name"
+              placeholder="Wpisz imię użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Nazwisko</label>
+            <input
+              type="text"
+              className="form-control"
+              value={user.surname}
+              name="surname"
+              placeholder="Wpisz nazwisko użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Login</label>
+            <input
+              type="text"
+              className="form-control"
+              value={user.login}
+              name="login"
+              placeholder="Wpisz nazwisko użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Hasło</label>
+            <input
+              type="password"
+              className="form-control"
+              value={user.password}
+              name="password"
+              placeholder="Wpisz hasło użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>E-mail</label>
+            <input
+              type="email"
+              className="form-control"
+              value={user.email}
+              name="email"
+              placeholder="Wpisz e-mail użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Miasto</label>
+            <input
+              type="city"
+              className="form-control"
+              value={user.city}
+              name="city"
+              placeholder="Wpisz miasto użytkownika"
+              onChange={onChange}
+            />
+          </div>
+          <div className="form-group">
+            <label>Miasto</label>
+            <select className="form-control" value={user.role}>
+              <option value="" disabled></option>
+              {roles.map((role, index) => {
+                return (
+                  <option key={index} value={role}>
+                    {role}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </form>
+      </div>
+      <div className="card-footer d-flex">
+        <button className="btn btn-primary mr-2" onClick={onSubmit}>
+          Dodaj użytkownika
+        </button>
+        <button className="btn btn-secondary" onClick={onCancel}>
+          Anuluj
+        </button>
+      </div>
     </div>
   );
 };
--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -1,26 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { withRouter } from "react-router";
-import { UserList } from "../components/Admin/admin_components";
+import { UserList, UserEdit } from "../components/Admin/admin_components";
 import { handleEdit, handleRemove } from "../components/Admin/admin_methods";
 import { useLoad } from "../utils/hooks";
 
 const SERVER_URL = process.env.REACT_APP_SERVER_URL || "http://localhost:3001";
 
 const USER_ROLES = ["user", "employee", "admin"];
+const EMPTY_USER_TEMPLATE = {
+  name: "",
+  surname: "",
+  login: "",
+  password: "",
+  email: "",
+  city: "",
+  role: "",
+};
 
 const AdminPage = () => {
+  const [user, setUser] = useState(EMPTY_USER_TEMPLATE);
   const [users, setUsers] = useLoad([], `${SERVER_URL}/users`);
+  const [selectedUser, setSelectedUser] = useState(null);
   const [isUserModalVisible, setIsUserModalVisible] = useState(false);
+  const handleUserSelection = (id) => {
+    const user = users.filter((u) => u._id === id);
+    if (user.length) {
+      setIsUserModalVisible(true);
+      setSelectedUser(user[0]);
+    } else {
+      setIsUserModalVisible(false);
+      setSelectedUser(null);
+    }
+  };
 
   return (
     <div className="row">
       <UserList
+        performEdit={(e) => handleUserSelection(e.target.dataset.id)}
         onEdit={(e) => console.log(e.target.dataset.id)}
         onRemove={(e) => handleRemove(e.target.dataset.id, setUsers)}
         users={users}
         isModalVisible={isUserModalVisible}
         setIsModalVisible={setIsUserModalVisible}
       />
+      {isUserModalVisible && selectedUser && (
+        <UserEdit
+          onSubmit={() => console.log("ok")}
+          onCancel={() => {
+            setIsUserModalVisible(false);
+          }}
+          onChange={(e) =>
+            setUser({ ...user, [e.target.name]: e.target.value })
+          }
+          user={selectedUser}
+          roles={USER_ROLES}
+        />
+      )}
     </div>
   );
 };
