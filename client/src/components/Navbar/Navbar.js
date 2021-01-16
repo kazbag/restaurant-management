c1b43029ea4665922f965ac068368f4e783746a4
Kazimierz Bagrowski <kazimierz.bagrowski@microsoft.wsei.edu.pl>
2021-01-16 21:20:18 +0100

    fix navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -8,137 +8,99 @@ const Navbar = (props) => {
   return (
     <AuthContext.Consumer>
       {(context) => (
-        <>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-toggle="collapse"
-              data-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <ul className="navbar-nav">
-                {isAuthenticated && userRole === "admin" && (
-                  <>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" toexact to="/">
-                        Strona główna
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/kitchen">
-                        Kuchnia
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/codes">
-                        Kody
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/menu">
-                        Menu
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/admin">
-                        Admin
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/logout">
-                        Wyloguj
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-                {isAuthenticated && userRole === "employee" && (
-                  <>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" exact to="/">
-                        Strona główna
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/kitchen">
-                        Kuchnia
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/logout">
-                        Wyloguj
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-
-                {isAuthenticated && userRole === "user" && (
-                  <>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" exact to="/">
-                        Strona główna
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/products">
-                        Zamów
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/about">
-                        O Nas
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/gallery">
-                        Galeria
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/logout">
-                        Wyloguj
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-                {!isAuthenticated && (
-                  <>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" exact to="/">
-                        Strona główna
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/login">
-                        Logowanie
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/about">
-                        O Nas
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/register">
-                        Rejestracja
-                      </NavLink>
-                    </li>
-                    <li className="nav-item">
-                      <NavLink className="nav-link" to="/gallery">
-                        Galeria
-                      </NavLink>
-                    </li>
-                  </>
-                )}
-              </ul>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="/">
+            Restauracja
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavAltMarkup"
+            aria-controls="navbarNavAltMarkup"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              {isAuthenticated && userRole === "admin" && (
+                <>
+                  <NavLink className="nav-link" toexact to="/">
+                    Strona główna
+                  </NavLink>
+                  <NavLink className="nav-link" to="/kitchen">
+                    Kuchnia
+                  </NavLink>
+                  <NavLink className="nav-link" to="/codes">
+                    Kody
+                  </NavLink>
+                  <NavLink className="nav-link" to="/menu">
+                    Menu
+                  </NavLink>
+                  <NavLink className="nav-link" to="/admin">
+                    Admin
+                  </NavLink>
+                  <NavLink className="nav-link" to="/logout">
+                    Wyloguj
+                  </NavLink>
+                </>
+              )}
+              {isAuthenticated && userRole === "employee" && (
+                <>
+                  <NavLink className="nav-link" exact to="/">
+                    Strona główna
+                  </NavLink>
+                  <NavLink className="nav-link" to="/kitchen">
+                    Kuchnia
+                  </NavLink>
+                  <NavLink className="nav-link" to="/logout">
+                    Wyloguj
+                  </NavLink>
+                </>
+              )}
+              {isAuthenticated && userRole === "user" && (
+                <>
+                  <NavLink className="nav-link" exact to="/">
+                    Strona główna
+                  </NavLink>
+                  <NavLink className="nav-link" to="/products">
+                    Zamów
+                  </NavLink>
+                  <NavLink className="nav-link" to="/about">
+                    O Nas
+                  </NavLink>
+                  <NavLink className="nav-link" to="/gallery">
+                    Galeria
+                  </NavLink>
+                  <NavLink className="nav-link" to="/logout">
+                    Wyloguj
+                  </NavLink>
+                </>
+              )}
+              {!isAuthenticated && (
+                <>
+                  <NavLink className="nav-link" exact to="/">
+                    Strona główna
+                  </NavLink>
+                  <NavLink className="nav-link" to="/login">
+                    Logowanie
+                  </NavLink>
+                  <NavLink className="nav-link" to="/about">
+                    O Nas
+                  </NavLink>
+                  <NavLink className="nav-link" to="/register">
+                    Rejestracja
+                  </NavLink>
+                  <NavLink className="nav-link" to="/gallery">
+                    Galeria
+                  </NavLink>
+                </>
+              )}
             </div>
-          </nav>
-        </>
+          </div>
+        </nav>
       )}
     </AuthContext.Consumer>
   );
